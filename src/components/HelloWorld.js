e816ec1994723a13e027e40f04c36fe6c172e965
joaomartinssecundario2-rgb <joaomartinssecundario2@gmail.com>
2026-09-16 10:33:07 -0300

    Utilização de CSS com Componentes

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,10 +2,8 @@ import Frase from './Frase';
 function HelloWorld() {
   return (
     <div>
-      < Frase />
+      <Frase/>
       <h1>Meu primeiro componente!</h1>
-      < Frase />
-      < Frase />
     </div>
   );
 }
